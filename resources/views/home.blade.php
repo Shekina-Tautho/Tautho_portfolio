9cce5a46706c1f7f576997fabffea3d73a1bb21d
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-01-19 12:14:58 +0800

    use of included, sections, yield and content.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,14 @@
 <body>
     <h1>Welcome to my Portfolio</h1>
     <script src="script.js"></script>
+
+    @extends('layouts.content')
+
+    @section('title', 'Home')
+
+    @section('content')
+    <h1>Welcome to my Portfolio</h1>
+    <p>This is my personal website.</p>
+    @endsection
 </body>
 </html>
