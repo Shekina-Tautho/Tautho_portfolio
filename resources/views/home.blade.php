3775eebef427a48de853c2777083c3ddae2c729b
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-02-27 12:01:21 +0800

    lab activity 1: routes, middleware, and blade templates

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.content')
+
+@section('title', 'about')
+
+@section('content')
+<h1>Welcome to my Portfolio</h1>
+<p>This is my fourth portfolio</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
