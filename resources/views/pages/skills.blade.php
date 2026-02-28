2e4acf87c8ec328a0db77723b9c7b4f55509878a
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-02-28 19:07:30 +0800

    feat: implement Skills page with Blade template, SkillsController, routes, Bootstrap layout, and CSS styling

--- resources/views/pages/skills.blade.php
+++ resources/views/pages/skills.blade.php
@@ -6,5 +6,37 @@
 @endpush
 
 @section('content')
-    <h1>skills</h1>
+    <div class="container w-100 h-100">
+        <div class="row h-100 w-100 g-0">
+            <div class="col-6 d-flex flex-column mt-5 px-5 pt-5 align-items-center">
+                <div class="row d-flex justify-content-center px-5">
+                    @foreach ($skills as $skill)
+                        <h1 class="header red px-5">{{ $skill->header1}}</h1>
+                        <h1 class="header light-gray px-5">{{ $skill->header2}}<span class="header dark-gray">{{ $skill->header3}}</span></h1>
+                    @endforeach
+                </div>
+                <div class="row d-flex justify-content-center mt-4">
+                    <div class="col-8">
+                        @foreach ($skills as $skill)
+                            <p class="description mx-auto">{{ $skill->description }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 d-flex align-items-start">
+                <div class="row w-100">
+                    <div class="col-6 flex-column justify-content-center px-5">
+                        @foreach ($skills as $skill)
+                            <p class="item tech pt-3">{{ $skill->tech}}</p>
+                        @endforeach
+                    </div>
+                    <div class="col-6 flex-column justify-content-center mt-5">
+                        @foreach ($skills as $skill)
+                            <p class="item tool pt-3">{{ $skill->tool}}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
