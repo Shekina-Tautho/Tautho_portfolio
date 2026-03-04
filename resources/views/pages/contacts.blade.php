e71c2218f4654f59a9128b92b55c1b5a41df0fc5
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-03-05 04:41:59 +0800

    feat: implement contacts page with controller and styling

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -2,23 +2,26 @@
 
 @section('title', 'about')
 @push('css')
-    <link rel="stylesheet" href="{{ asset('css/projects.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contacts.css') }}">
 @endpush
 
 @section('content')
     <div class="container w-100 h-100">
-        <div class="row h-100 w-100 d-flex justify-content-center">
+        <div class="row d-flex flex-column align-items-center">
             <div class="row mt-5 d-flex justify-content-center">
                 @foreach ($contacts as $item)
-                    <h1 class="header red mb-5">{{ $item->header}}</h1>
+                    <h1 class="header red mb-5 mt-5 text-center">{{ $item->header }}</h1>
                 @endforeach
             </div>
-            <div class="row d-flex flex-column align-items-center">
+            <div class="row justify-content-center text-center">
                 @foreach ($contacts as $item)
-                    <p class="desc gray mt-5">{{ $item->link }}</p>
+                    <div class="col-auto">
+                        <span class="desc gray">
+                            {{ $item->link }}
+                        </span>
+                    </div>
                 @endforeach
             </div>
         </div>
-        
     </div>
 @endsection
